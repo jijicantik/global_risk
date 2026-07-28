fde17ab00fe3462dc02d97c3ce6e71d457b52ff1
Muazzinah <muazinah305@gmail.com>
2026-07-28 17:32:27 +0700

    Prepare for Railway deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production' || request()->server->has('HTTP_X_FORWARDED_PROTO')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
